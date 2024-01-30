98ab89e7adc303a502802c2cef75757cbdce9994
AmoyLeo731 <104699486+AmoyLeo731@users.noreply.github.com>
2024-01-30 11:15:54 -0600

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Lizhi Hu',
+  domain: 'welcome.Lizhi Hu',
+  author: 'Lizhi Hu',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
